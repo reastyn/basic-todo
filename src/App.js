5977b7cfe25890a7fdc66d7468b992d37fa6b9ab
Tomas Rokos <tomas@rokos.cz>
2018-06-15 19:21:49 +0200

    Component edits because of Flow, basic layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
+// @flow
 import React, { Component } from 'react';
 import './App.scss';
 
-class App extends Component {
+type Props = {
+
+}
+
+class App extends Component<Props> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter task-list">
+            <div class="box">
+              <h2 class="title">My tasks</h2>
+              <hr />
+              <div class="field has-addons">
+                <div class="control is-expanded">
+                  <input class="input" type="text" placeholder="New task" />
+                </div>
+                <div class="control">
+                  <a class="button is-primary">
+                    Add task
+                        </a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-content">
+                <div class="content">
+
+                  <label class="checkbox is-size-5">
+                    <input type="checkbox" />
+                    Remember me
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
