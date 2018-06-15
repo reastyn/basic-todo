0c2fb875a5a7646c900140b2e5e3e989b4f9259a
Tomas Rokos <tomas@rokos.cz>
2018-06-15 19:21:31 +0200

    Added Bulma CSS Framework to the project

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -188,6 +188,12 @@ module.exports = {
               },
             ],
           },
+          // SCSS parser
+          {
+            test: /\.scss$/,
+            include: paths.appSrc,
+            use: [require.resolve('style-loader'), require.resolve('css-loader'), require.resolve('sass-loader')]
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
